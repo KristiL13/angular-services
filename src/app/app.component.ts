4bafb27dce02a6b2cef453042472fd978c841769
Kristi Laur <kristi@heisi.eu>
2022-05-03 13:43:10 +0300

    Add and implement Account service for data
    This doesn't work properly yet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
+  constructor(private accountsService: AccountsService) { }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit(): void {
+    // Since accounts in the service is an array, it is a reference type.
+    // And we will get here the exact same array as is in the service file.
+    this.accounts = this.accountsService.accounts;
   }
 }
