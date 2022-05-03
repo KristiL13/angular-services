9878c5b047eda507ea9a939ec3604e62aa99e38f
Kristi Laur <kristi@heisi.eu>
2022-05-03 13:29:01 +0300

    Inject LoggingService into components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,22 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-// import { LoggingService } from '../logging.service';
+import { LoggingService } from '../logging.service'; // telling TS where this service comes from
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService] // Providing the service for Angular.
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService) { } // Dependency injection of the service in Angular.
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-
-    // This works, but this is wrong in Angular!!
-    // const service = new LoggingService();
-    // service.logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
